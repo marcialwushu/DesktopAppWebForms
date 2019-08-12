f2efff2f23130283cb03d2619c2f69edadae8482
marcialwushu <marcialwushu@gmail.com>
2019-08-12 16:08:01 -0300

    MySecondCustomControl added

---
+++ FlatDesktop/MySecondCustomControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlatDesktop
+{
+    public partial class MySecondCustomControl : UserControl
+    {
+        public MySecondCustomControl()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
